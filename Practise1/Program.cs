58116a535ee3bb9ea01025c20fbe5bd7f4ba359b
jmcsilva <joaomiguelcsilva@gmail.com>
2017-03-20 14:16:26 +1100

    1stCommit

---
+++ Practise1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practise1
+{
+    class Program
+    {
+        public static int[] values1 = { 1, 2, 3, 9 };
+        public static int[] values2 = { 1, 2, 4, 4 };
+        static void Main(string[] args)
+        {
+            if (HasSumOfPairs(values1, 8))
+            {
+                Console.WriteLine("1st  test FOUND");
+            }
+            else
+                Console.WriteLine("1st  test NOTFOUND");
+
+            if (HasSumOfPairs(values2, 8))
+            {
+                Console.WriteLine("2nd  test FOUND");
+            }
+            else
+                Console.WriteLine("2nd  test NOTFOUND");
+
+            Console.ReadLine();
+        }
+
+        public static bool HasSumOfPairs(int[] values, int sum)
+        {
+            int low = 0;
+            int high = values.Length -1; 
+
+            int res = 0;
+            
+            while (low < high)
+            {
+
+                if (low < 0 || high < 0)
+                {
+                    Console.WriteLine("OUT of Bounds");
+                    return false;
+                }
+
+
+                res = values[low] + values[high];
+                if (res == sum)
+                {
+                    return true;
+                }
+                else
+                {
+                    if (res > sum)
+                    {
+                        high--;
+                    }
+                    else if (res < sum)
+                    {
+                        low++;
+                    }
+                    
+                }    
+            }
+
+            return true;
+
+        }
+    }
+}
